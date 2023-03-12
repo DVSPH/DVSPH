fa36f73597a6cff14b5bbd77208ebda0b99b5af4
M0JGX <85508750+M0JGX@users.noreply.github.com>
2023-03-12 16:50:54 +0000

    Update TalkGroups.php
    Added Table header that was missing as it was embedded in the original site (lyrg.co.uk)

--- TalkGroups.php
+++ TalkGroups.php
@@ -1,9 +1,15 @@
 <?php
+
+//Quick and simple table to display the Official Talk Group feed from DVSPH DMR Network
+//Feel free to customise but remember to please give credit to DVSHP.net for usage
+
 $json_data = file_get_contents("https://dvsph.net/api/TalkGroups.json");
 
 $data = json_decode($json_data, true);
 
 $TalkGroups = $data['TalkGroups'];
+
+echo '<table>';
 
 foreach($TalkGroups as $row){
 
@@ -11,4 +17,5 @@ foreach($TalkGroups as $row){
     if (isset($link)) {$description = '<a href="'.$row["Link"].'" target="_blank">'. $row["Description"];} else {$description = $row["Description"];} 
 
    echo "<tr><th>".$row["Slot"].'</th><th><a href="http://stats.dvscotland.net/monitor.php?filter=tg&tgid='.$row["TalkGroup"].'" target="_blank">'.$row["TalkGroup"].'</th><th>'. $description."</a></th></tr>";}
+   echo '</table></br><a href="https://dvsph.net/talk-groups">Credit: DVSPH.net</a>';  
 ?>
