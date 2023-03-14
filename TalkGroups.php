b18dc9b05d2c0fa861e677bc82d3f1c27214a060
M0JGX <85508750+M0JGX@users.noreply.github.com>
2023-03-14 18:58:29 +0000

    Updated stats website to reflect new domain
    Changed from stats.dvsscotland.net to stats.dvsph.net

--- TalkGroups.php
+++ TalkGroups.php
@@ -16,6 +16,6 @@ foreach($TalkGroups as $row){
     $link = $row["Link"];
     if (isset($link)) {$description = '<a href="'.$row["Link"].'" target="_blank">'. $row["Description"];} else {$description = $row["Description"];} 
 
-   echo "<tr><th>".$row["Slot"].'</th><th><a href="http://stats.dvscotland.net/monitor.php?filter=tg&tgid='.$row["TalkGroup"].'" target="_blank">'.$row["TalkGroup"].'</th><th>'. $description."</a></th></tr>";}
+   echo "<tr><th>".$row["Slot"].'</th><th><a href="https://stats.dvsph.net/monitor.php?filter=tg&tgid='.$row["TalkGroup"].'" target="_blank">'.$row["TalkGroup"].'</th><th>'. $description."</a></th></tr>";}
    echo '</table></br><a href="https://dvsph.net/talk-groups">Credit: DVSPH.net</a>';  
 ?>
