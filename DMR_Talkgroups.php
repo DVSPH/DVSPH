cd4dae9bfd7bb0c85b0278d26bbd3974ead0ac0a
M0JGX <85508750+M0JGX@users.noreply.github.com>
2023-08-22 18:53:33 +0100

    Add files via upload

---
+++ DMR_Talkgroups.php
@@ -0,0 +1,40 @@
+<table>
+<thead>
+
+<tr>
+    <th style="width:10%">Slot</th>
+  	<th style="width:25%">Talk Group</th>
+    <th style="width:65%">Description</th>
+    
+     
+</tr>
+</thead>
+  <tbody>
+
+
+<?php
+$json_data = file_get_contents("https://dvsph.net/api/TalkGroups.json");
+
+$data = json_decode($json_data, true);
+
+$items = $data['TalkGroups'];
+
+foreach($items as $row){
+
+  if ($row["Interlink"] =="") {
+      
+     echo "<tr><td>".$row["Slot"].'</td><td>'.$row["TalkGroup"].'</td><td>'.$row["Description"]."</td></tr>";
+      
+      
+      
+    } else { 
+  
+
+   echo "<tr><td>".$row["Slot"].'</td><td>'.$row["TalkGroup"].'</td><td><a href="#" data-toggle="tooltip" title="Linked to: '.$row["Interlink"].'">'.$row["Description"]."</a></td></tr>";}
+  
+  
+}
+   
+?>
+ </tbody>
+</table>
